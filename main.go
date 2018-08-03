33f29425534514ec2289888d96a812983b981401
Zak <zak@beattiecole.com>
2018-08-03 07:45:15 -0700

    removed flag

--- main.go
+++ main.go
@@ -2,28 +2,28 @@ package main
 
 import (
 	"crypto/sha256"
-	"flag"
-	"fmt"
+	"encoding/hex"
+	"sync"
 )
+
 type block struct {
-	Index int
+	Index     int
 	Timestamp string
-	Hash string
-	Parent string
+	Hash      string
+	Parent    string
 }
 
 var Minichain []Block
 
 var mutex = &sync.Mutex{}
 
-var input string = flag.StringVar(&input, "i", "fubu for us by us", "a string")
+// var input string = flag.StringVar(&input, "i", "fubu for us by us", "a string")
 
-var height int
-flag.IntVar(&height, "n", 12, "a number")
-flag.Parse()
+// var height int
+// flag.IntVar(&height, "n", 12, "a number")
 
 func calcHash(block Block) string {
-	record := string(block.Index) + block.Timestamp + block.Parent 
+	record := string(block.Index) + block.Timestamp + block.Parent
 	h := sha256.New()
 	h.Write([]byte(record))
 	hashed := h.Sum(nil)
